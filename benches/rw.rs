36a20d3c67c4fae4d338208770bd9b36a7437738
Mansur Azatbek <mansur62624@gmail.com>
2024-11-12 12:49:59 +0500

    test: add benchmarks

--- benches/rw.rs
+++ benches/rw.rs
@@ -12,21 +12,66 @@ fn generate_large_string() -> String {
         .collect()
 }
 
-fn bench_movetex_with_write(c: &mut Criterion) {
-    let movetex = Arc::new(Movetex::new(generate_large_string()));
+fn bench_mutex_with_write_small(c: &mut Criterion) {
+    let mutex = Arc::new(Mutex::new(0u128));
 
-    c.bench_function("Movetex read/write", |b| {
+    c.bench_function("Mutex read/write small", |b| {
+        b.iter(|| {
+            let m = Arc::clone(&mutex);
+            thread::scope(|s| {
+                for _ in 0..10 {
+                    let m = Arc::clone(&m);
+                    s.spawn(move || {
+                        drop(m.lock().unwrap());
+                    });
+                }
+                let m = Arc::clone(&m);
+                s.spawn(move || {
+                    let mut data = m.lock().unwrap();
+                    *data = 1;
+                });
+            });
+        })
+    });
+}
+
+fn bench_movetex_with_write_small(c: &mut Criterion) {
+    let movetex = Arc::new(Movetex::new(0u128));
+
+    c.bench_function("Movetex read/write small", |b| {
         b.iter(|| {
             let m = Arc::clone(&movetex);
             thread::scope(|s| {
-                // Запуск потоков на чтение
                 for _ in 0..10 {
                     let m = Arc::clone(&m);
                     s.spawn(move || {
                         let _ = m.read();
                     });
                 }
-                // Одновременная запись длинной строки
+                let m = Arc::clone(&m);
+                s.spawn(move || {
+                    m.write(|data| {
+                        *data = 1;
+                    });
+                });
+            });
+        })
+    });
+}
+
+fn bench_movetex_with_write(c: &mut Criterion) {
+    let movetex = Arc::new(Movetex::new(generate_large_string()));
+
+    c.bench_function("Movetex read/write large", |b| {
+        b.iter(|| {
+            let m = Arc::clone(&movetex);
+            thread::scope(|s| {
+                for _ in 0..10 {
+                    let m = Arc::clone(&m);
+                    s.spawn(move || {
+                        let _ = m.read();
+                    });
+                }
                 let m = Arc::clone(&m);
                 s.spawn(move || {
                     m.write(|data| {
@@ -41,18 +86,16 @@ fn bench_movetex_with_write(c: &mut Criterion) {
 fn bench_mutex_with_write(c: &mut Criterion) {
     let mutex = Arc::new(Mutex::new(generate_large_string()));
 
-    c.bench_function("Mutex read/write", |b| {
+    c.bench_function("Mutex read/write large", |b| {
         b.iter(|| {
             let m = Arc::clone(&mutex);
             thread::scope(|s| {
-                // Запуск потоков на чтение
                 for _ in 0..10 {
                     let m = Arc::clone(&m);
                     s.spawn(move || {
                         drop(m.lock().unwrap());
                     });
                 }
-                // Одновременная запись длинной строки
                 let m = Arc::clone(&m);
                 s.spawn(move || {
                     let mut data = m.lock().unwrap();
@@ -63,10 +106,58 @@ fn bench_mutex_with_write(c: &mut Criterion) {
     });
 }
 
+fn bench_movetex_with_multiple_writes(c: &mut Criterion) {
+    let movetex = Arc::new(Movetex::new(generate_large_string()));
+
+    c.bench_function("Movetex multiple writes", |b| {
+        b.iter(|| {
+            let m = Arc::clone(&movetex);
+            thread::scope(|s| {
+                for _ in 0..10 {
+                    let m = Arc::clone(&m);
+                    s.spawn(move || {
+                        m.write(|data| {
+                            *data = generate_large_string();
+                        });
+                    });
+                }
+                let m = Arc::clone(&m);
+                s.spawn(move || {
+                    let _ = m.read();
+                });
+            });
+        })
+    });
+}
+
+fn bench_mutex_with_multiple_writes(c: &mut Criterion) {
+    let mutex = Arc::new(Mutex::new(generate_large_string()));
+
+    c.bench_function("Mutex multiple writes", |b| {
+        b.iter(|| {
+            let m = Arc::clone(&mutex);
+            thread::scope(|s| {
+                for _ in 0..10 {
+                    let m = Arc::clone(&m);
+                    s.spawn(move || {
+                        let mut data = m.lock().unwrap();
+                        *data = generate_large_string();
+                    });
+                }
+                let m = Arc::clone(&m);
+                s.spawn(move || {
+                    drop(m.lock().unwrap());
+                });
+            });
+        })
+    });
+}
+
 criterion_group! {
     name = benches;
     config = Criterion::default().measurement_time(std::time::Duration::from_secs(6));
-    targets = bench_mutex_with_write, bench_movetex_with_write
+    targets = bench_mutex_with_write_small, bench_movetex_with_write_small,
+    bench_mutex_with_write, bench_movetex_with_write, bench_mutex_with_multiple_writes, bench_movetex_with_multiple_writes
 }
 
 criterion_main!(benches);
