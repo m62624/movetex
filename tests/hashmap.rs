446171944f042646b4b8a287cdcac0023259b76f
Mansur Azatbek <mansur62624@gmail.com>
2024-11-12 21:19:42 +0500

    test: change test

--- tests/hashmap.rs
+++ tests/hashmap.rs
@@ -8,7 +8,7 @@ use std::time::{Duration, Instant};
 fn generate_random_string() -> String {
     thread_rng()
         .sample_iter(&Alphanumeric)
-        .take(10) // длина строки 10 символов
+        .take(10)
         .map(char::from)
         .collect()
 }
@@ -19,10 +19,9 @@ fn test_movetex_parallel_write_consistency() {
     let mut reference_map = HashMap::new();
     let mut prefilled_values = Vec::new();
 
-    // Generate key-value pairs beforehand
     for i in 0..10 {
-        for j in 0..100 {
-            let key = i * 100 + j;
+        for j in 0..1000 {
+            let key = i * 1000 + j;
             let value = generate_random_string();
             prefilled_values.push((key, value.clone()));
             reference_map.insert(key, value);
@@ -38,7 +37,7 @@ fn test_movetex_parallel_write_consistency() {
     // Barrier to synchronize threads
     let barrier = Arc::new(Barrier::new(11)); // 10 writer threads + 1 main thread
 
-    // Создаем 10 потоков, каждый записывает по 100 элементов
+    // Создаем 10 потоков, каждый записывает по 1000 элементов
     thread::scope(|s| {
         for i in 0..10 {
             let m = Arc::clone(&movetex_map_writer);
@@ -46,14 +45,14 @@ fn test_movetex_parallel_write_consistency() {
             let c = Arc::clone(&barrier);
 
             s.spawn(move || {
-                for (key, value) in values.iter().skip(i * 100).take(100) {
+                for (key, value) in values.iter().skip(i * 1000).take(1000) {
                     loop {
                         if m.write(|map| {
                             map.insert(*key, value.clone());
                         }) {
                             break;
                         }
-                        thread::yield_now(); // Yield to allow other threads to proceed
+                        thread::yield_now();
                     }
                 }
                 c.wait(); // Wait at the barrier
@@ -62,25 +61,27 @@ fn test_movetex_parallel_write_consistency() {
 
         barrier.wait(); // Main thread waits for all writer threads
         println!("All writes to movetex are completed.");
-        println!(" movetex : {:#?}", movetex_map_reader.read());
     });
 
     // Чтение через 5 секунд и проверка идентичности
     let start_time = Instant::now();
+    let time_limit = Duration::from_secs(60);
 
     let reader_thread = thread::spawn(move || {
         thread::sleep(Duration::from_secs(5)); // Задержка перед началом чтения
         loop {
             let elapsed = start_time.elapsed();
-            if elapsed > Duration::from_secs(60) {
-                panic!("Test failed: Took longer than 60 seconds to complete");
+            if elapsed > time_limit {
+                panic!(
+                    "Test failed: Took longer than {} seconds to complete",
+                    time_limit.as_secs()
+                );
             }
 
             if *movetex_map_reader.read() == reference_map {
                 println!("Test passed: HashMaps are identical.");
                 return;
             }
-            thread::sleep(Duration::from_millis(100));
         }
     });
 
